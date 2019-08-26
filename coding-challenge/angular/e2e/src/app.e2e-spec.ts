56fa455bbf7b5109572fe7f377dce957e1fd6c9d
manuel <manuel@Deepthought.fritz.box>
2019-08-26 15:14:02 +0200

    end-to-end test for char-grouper form (challenge 007)

--- coding-challenge/angular/e2e/src/app.e2e-spec.ts
+++ coding-challenge/angular/e2e/src/app.e2e-spec.ts
@@ -1,16 +1,28 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('CharGrouper App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display title text(s)', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to codingchallenge!');
+    expect(page.getTitleText()).toEqual('CharGrouper');
+    expect(page.getSubTitleText()).toEqual('Enter a string on the left and see the result on the right');
+  });
+
+  it('should group input text', async () => {
+    page.navigateTo();
+    expect(page.getSubmitIsEnabled()).toBe(false);
+
+    page.enterInputText('abzuaaissna');
+    expect(page.getSubmitIsEnabled()).toBe(true);
+
+    await page.clickSubmitButton();
+    expect(page.getOutputText()).toBe('a4binssuz');
   });
 
   afterEach(async () => {
--- coding-challenge/angular/e2e/src/app.po.ts
+++ coding-challenge/angular/e2e/src/app.po.ts
@@ -1,11 +1,42 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
+
+  inputString: ElementFinder;
+  outputString: ElementFinder;
+  submitButton: ElementFinder;
+
+  constructor() {
+    this.inputString = element(by.css('[name="input"]'));
+    this.outputString = element(by.css('[name="output"]'));
+    this.submitButton = element(by.css('[type="submit"]'));
+  }
+
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
   getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+    return element(by.css('app-root h2')).getText() as Promise<string>;
+  }
+
+  getSubTitleText() {
+    return element(by.css('app-root h4')).getText() as Promise<string>;
+  }
+
+  getSubmitIsEnabled() {
+    return this.submitButton.isEnabled();
+  }
+
+  enterInputText(inputText: string) {
+    this.inputString.sendKeys(inputText);
+  }
+
+  async clickSubmitButton() {
+    await this.submitButton.click();
+  }
+
+  getOutputText() {
+    return this.outputString.getAttribute('value');
   }
 }
